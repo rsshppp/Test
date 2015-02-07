d7b334963a28eefcfce5a6035502151a013dd8fb
silverbullet2 <panwangle@gmail.com>
2015-02-07 11:43:35 +0800

    Update Test.java

--- Test.java
+++ Test.java
@@ -3,11 +3,13 @@ package com.steven;
 public class Test {
 
 	public static void main(String[] args) {
-		//½m²ßGit+Github»·ºİ¾Ş§@
-		//½Ğ¯d¤U§A­Ó¤Hªº¸ê°T¥Nªí§Ú­Ì¦@¦P½s¿è¤Fcode
-		System.out.println("Steven¨ì¦¹¤@¹C!!!");
+		//ç·´ç¿’Git+Githubé ç«¯æ“ä½œ
+		//è«‹ç•™ä¸‹ä½ å€‹äººçš„è³‡è¨Šä»£è¡¨æˆ‘å€‘å…±åŒç·¨è¼¯äº†code
+		System.out.println("Stevenåˆ°æ­¤ä¸€éŠ!!!");
 		
-		System.out.println("Pasion¨ì¦¹¤@¹C!!!");
+		System.out.println("Pasionåˆ°æ­¤ä¸€éŠ!!!");
+		
+		System.out.println("Noahåˆ°æ­¤ä¸€éŠ!!!");
 	}
 
 }
