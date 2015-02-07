6acd980df229adc7f3db7fae1cab14225cbe37d7
Steven <rsshppp@gmail.com>
2015-02-07 11:14:42 +0800

    Test.java for coding together.

---
+++ Test.java
@@ -0,0 +1,12 @@
+package com.steven;
+
+public class Test {
+
+	public static void main(String[] args) {
+		//練習Git+Github遠端操作
+		//請留下你個人的資訊代表我們共同編輯了code
+		System.out.println("Steven到此一遊!!!");
+
+	}
+
+}
